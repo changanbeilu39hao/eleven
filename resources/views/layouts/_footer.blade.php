df1a0e2642056e0f0c05e5e50c858437f836e964
hengzh1 <spylxz@hotmail.com>
2020-03-12 10:01:52 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="float-center">
+        <a href="/about">编码By.</a>
+    </div>
+</footer>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,7 +4,7 @@
     <div class="jumbotron">
         <h1>Eleven</h1>
         <p class="lead">
-            这是一个伴奏交易平台。
+            这是一个音乐有偿分享的平台。
         </p>
         <p>
             一切将从这里开始。
