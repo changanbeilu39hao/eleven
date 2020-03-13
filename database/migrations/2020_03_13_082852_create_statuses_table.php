f3a0d10e2c80fb4b2b3e0d0feef697684f67ef69
hengzh1 <spylxz@hotmail.com>
2020-03-13 16:56:03 +0800

    动态数据模型

---
+++ database/migrations/2020_03_13_082852_create_statuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('content');
+            $table->string('img_path', 191);
+            $table->string('audio_path', 191);
+            $table->bigInteger('user_id')->index();
+            $table->timestamps();
+            $table->index(['created_at']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
