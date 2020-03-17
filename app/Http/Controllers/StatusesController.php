1fe24773fcd2c107aac8cc1bc6c6dd8fbe8cb694
hengzh1 <spylxz@hotmail.com>
2020-03-17 15:22:40 +0800

    修改音频

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -18,11 +18,12 @@ class StatusesController extends Controller
         
         $this->validate($request, [
             'img' => 'required|mimetypes:image/jpeg,image/png,image/bmp',
-            'audio' => 'required|mimes:mp3',
+            'audio' => 'required|mimetypes:audio/mpeg',
             'content' => 'max:140',
         ],[
             'img.required'  => '请选择图片',
             'audio.required' => '请选择音频',
+            'audio.mimetypes' => '音频必须是 mp3 格式',
             'content.max' => '介绍内容过长'
         ]);
         $img = $request->file('img')->store('public/cover');
